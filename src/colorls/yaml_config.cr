349bdc76e718be0f9827e88402f9ab6ecb66d906
Kirk Haines <wyhaines@gmail.com>
2026-01-28 22:02:50 -0700

    Oops. All of these files need to be read at compile time, not at execution-time.

--- src/colorls/yaml_config.cr
+++ src/colorls/yaml_config.cr
@@ -4,26 +4,33 @@ module Colorls
   class YamlConfig
     YAML_DIR = Path[__DIR__].parent / "yaml"
 
+    # Embed all YAML files at compile time so the binary is self-contained.
+    EMBEDDED_YAML = {
+      "dark_colors.yaml"    => {{ read_file("#{__DIR__}/../yaml/dark_colors.yaml") }},
+      "light_colors.yaml"   => {{ read_file("#{__DIR__}/../yaml/light_colors.yaml") }},
+      "files.yaml"          => {{ read_file("#{__DIR__}/../yaml/files.yaml") }},
+      "file_aliases.yaml"   => {{ read_file("#{__DIR__}/../yaml/file_aliases.yaml") }},
+      "folders.yaml"        => {{ read_file("#{__DIR__}/../yaml/folders.yaml") }},
+      "folder_aliases.yaml" => {{ read_file("#{__DIR__}/../yaml/folder_aliases.yaml") }},
+    }
+
     def initialize(@filename : String)
       @user_config_path = Path.home / ".config" / "colorls" / @filename
     end
 
     # Load YAML config, merging user overrides if present.
-    # If aliase is true, non-hex values are returned as-is (symbol references in Ruby;
-    # in Crystal we just keep the string value).
     def load(aliase : Bool = false) : Hash(String, String)
-      yaml = read_file(YAML_DIR / @filename)
+      yaml = parse_yaml(EMBEDDED_YAML[@filename])
 
       if File.exists?(@user_config_path)
-        user_yaml = read_file(@user_config_path)
+        user_yaml = parse_yaml(File.read(@user_config_path))
         yaml.merge!(user_yaml)
       end
 
       yaml
     end
 
-    private def read_file(path : Path | String) : Hash(String, String)
-      content = File.read(path.to_s)
+    private def parse_yaml(content : String) : Hash(String, String)
       parsed = YAML.parse(content)
       result = {} of String => String
       parsed.as_h.each do |k, v|
